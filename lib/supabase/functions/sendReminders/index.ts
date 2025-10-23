9e13b878c783040f52260304bc5fd4b460f74ed0
Derin <aderinsolasamashimi@gmail.com>
2025-10-23 11:44:35 +0100

    fix: still fixing errors

--- lib/supabase/functions/sendReminders/index.ts
+++ lib/supabase/functions/sendReminders/index.ts
@@ -1,4 +1,10 @@
 // supabase/functions/sendReminders/index.ts
+// @ts-ignore
+
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
+
+
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient(Deno.env.get("SUPABASE_URL")!, Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!);
